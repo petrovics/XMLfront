14cf525ba9b90e7bd87fffe810cc54c387799d01
Stasa <stasapetrovicns@gmail.com>
2023-03-28 11:07:56 +0200

    changes to index page

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import Link from "next/link"
+import styles from '@/styles/Header.module.css'
+
+const Header = () => {
+
+    return <div id={styles.header}>
+        <div className="left">
+
+        </div>
+        <div className="right">
+            <Link href={'/login'}>Login</Link>
+            <Link href={'/registration'}>Registration</Link>
+        </div>
+    </div>
+}
+
+export default Header
